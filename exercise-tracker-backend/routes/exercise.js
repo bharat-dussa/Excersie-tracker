fdfff3893ab67d1a50496466389d99bbcd8a61d7
bharat-dussa <bharatdussa@gmail.com>
2021-05-13 02:47:43 +0530

    second commit

--- exercise-tracker-backend/routes/exercise.js
+++ exercise-tracker-backend/routes/exercise.js
@@ -8,7 +8,7 @@ router.route('/').get((req, res) => {
 })
 
 // adding the excersie data
-router.route('/adding', (req, res) => {
+router.route('/adding').post((req, res) => {
     let errors = {};
     Excerise.findOne({
         username: req.body.username
@@ -37,11 +37,31 @@ router.route('/adding', (req, res) => {
 
 // getting all the data
 
-router.get('/:id').get((req,res)=>{
+router.route('/:id').get((req,res)=>{
     Excersie.findByIdAndDelete(req.params.id)
         .then(()=>res.json('Excersie'))
         .catch(err => res.status(400).json(err => err))
 })
 
+router.route('/update/:id').post((req,res) => {
+    const { username, description, duration, date } = req.body;
+    console.log("🚀 ~ file: exercise.js ~ line 48 ~ router.route ~ date", date)
+    console.log("🚀 ~ file: exercise.js ~ line 48 ~ router.route ~ duration", duration)
+    console.log("🚀 ~ file: exercise.js ~ line 48 ~ router.route ~ description", description)
+    console.log("🚀 ~ file: exercise.js ~ line 48 ~ router.route ~ username", username)
+    
+    Excerise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+        .then(()=> res.json(`exercise updated successfully`))
+        .catch((err)=> res.status(400).json(`error in uploading the data ${err}`))
+    })
+    .catch(err => res.status(400).json(`Error ${err}`))
+})
 
 module.exports = router;
